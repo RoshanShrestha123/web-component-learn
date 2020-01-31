96c3524343c5474be42a50b42e1fe6df521902b9
RoshanShrestha123 <roshan.shrestha4u@gmail.com>
2020-01-31 16:11:41 +0545

    added shadow dom and style implemented

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import './news-article.js';
-
 window.addEventListener('load',()=>{
-    // console.log(" this is working");
     const main = document.querySelector('main');
-    // const news ;
     const URL ='https://jsonplaceholder.typicode.com/todos/';
     fetch(URL)
     .then(response => response.json())
@@ -12,13 +9,10 @@ window.addEventListener('load',()=>{
     });
     function displayResult(data) {
         console.log(data);
-        
-       data.forEach(data => {
+        data.forEach(data => {
            const el = document.createElement('news-article');
            el.news = data;
            main.appendChild(el);
        }) 
     }
-
-
 })
--- news-article.js
+++ news-article.js
@@ -1,6 +1,18 @@
 class NewsArticle extends HTMLElement{
+    constructor(){
+        super();
+        this.root = this.attachShadow({mode:'open'});
+    }
+
     set news(news){
-        this.innerHTML = `<div>
+        this.root.innerHTML = `
+        <style>
+            h3 {
+                color:black;
+                font-family: arial;
+            }
+        </style>
+        <div>
             <h3> ${news.title} </h3>
             <hr>
         </div>`
